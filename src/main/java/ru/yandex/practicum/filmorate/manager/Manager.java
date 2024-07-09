01b8114d181d77ccf82f50b0d6d32ac30f533a6d
YG <yasha.golotin@mail.ru>
2024-07-09 22:22:12 +0300

    update manager v1.05

--- src/main/java/ru/yandex/practicum/filmorate/manager/Manager.java
+++ src/main/java/ru/yandex/practicum/filmorate/manager/Manager.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.manager;
 
 
 import lombok.Data;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
